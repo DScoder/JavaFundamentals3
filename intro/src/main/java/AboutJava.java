7def7099d7284ed0d996e52e30a019ddcefd6d8b
Ilya Vasilchenko <lawwwer@yahoo.com>
2016-08-20 12:34:11 +0300

    second commit

--- intro/src/main/java/AboutJava.java
+++ intro/src/main/java/AboutJava.java
@@ -2,8 +2,14 @@
  * Created by DScoder on 19.08.2016.
  */
 public class AboutJava {
-
+    int x1 = 123456789;
+    int x2 = 999999999;
+    float x3 = (float) x1;
+    float x4 = (float) x2;
+Integer
     public void call(){
         System.out.println("Java уже тут!!!");
+        System.out.println(x3);
+        System.out.println(x4);
     }
 }
--- intro/src/main/java/CommandArgs.java
+++ intro/src/main/java/CommandArgs.java
@@ -5,6 +5,8 @@ public class CommandArgs {
     public static void main(String[] args) {
         for (int i = 0; i < args.length; i++) {
             System.out.println("Name" + i + ": " + args[i]);
+
+
         }
     }
 }
